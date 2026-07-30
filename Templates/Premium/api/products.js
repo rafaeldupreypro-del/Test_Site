a0be3b80b092ec6281e1403200894faa30b7371a
Rafael Duprey <rafaeldupreypro@gmail.com>
2026-07-30 02:30:15 +0200

    Modif premium : test stripe produits

--- Templates/Premium/api/products.js
+++ Templates/Premium/api/products.js
@@ -5,7 +5,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
     if (req.method !== 'GET') {
-        return res.status(405.json({ error: 'Méthode non autorisée' }));
+        return res.status(405).json({ error: 'Méthode non autorisée' }); 
     }
 
     try {
